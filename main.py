14131f6f6af5fddb757fd5b38e59657ee7acf5b5
pearl178 <89487585+pearl178@users.noreply.github.com>
2022-04-28 11:45:33 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,37 @@
+
+import random
+import game_data
+import art
+src = game_data.data
+length = len(src)
+end_game = False
+a_dic = src[random.randint(0,50)]
+score = 0
+print(art.logo)
+while not end_game:
+  b_dic = src[random.randint(0,50)]
+  print(f"Compare A: {a_dic['name']}, a {a_dic['description']}, from {a_dic['country']} ")
+  print(art.vs)
+  print(f"Against B:{b_dic['name']}, a {b_dic['description']}, from {b_dic['country']}")
+  answer = input("Who has more followers? Type 'A' or 'B': ").lower()
+  #replit.clear()
+  a_follower = a_dic['follower_count']
+  b_follower = b_dic['follower_count']
+  
+  if a_follower>b_follower:
+    if answer == 'a':
+      correct = True
+    else:
+      correct = False
+  if a_follower<b_follower:
+    if answer == 'b':
+      correct = True
+      a_dic = b_dic
+    else:
+      correct = False
+  if correct:
+    score += 1
+    print(f"You're right! Current score:{score}")
+  else:
+    print(f"Sorry,that's wrong. Final score:{score}")
+    end_game = True
